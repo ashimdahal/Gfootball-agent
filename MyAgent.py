f35b11fc7812ef04f82b56218c4e66ce151957a2
ashimdahal <bboxchmpx@gmail.com>
2020-11-06 19:59:57 +0545

    Agent number one

---
+++ MyAgent.py
@@ -0,0 +1,22 @@
+import gfootball.env as football_env
+
+env = football_env.create_environment(
+                                env_name='11_vs_11_stochastic', 
+                                representation='raw',
+                                render=True)
+print(env.action_space.sample())
+
+# for episodes in range(4):
+#     obs = env.reset()
+#     for t in range(125):
+#         env.render()
+#         print(obs)
+#         action = env.action_space.sample()
+#         obs, reward, done, info = env.step(action)
+#         if done:
+#             print("Episode finished after {} timesteps".format(t+1))
+#             break
+env.close()
+
+
+
